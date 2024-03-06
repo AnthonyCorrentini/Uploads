135903a3b86d834f200167df96a77fb9fafe9b32
AnthonyCorrentini <162418142+AnthonyCorrentini@users.noreply.github.com>
2024-03-06 08:07:31 +0100

    Create Mini_Biblioteca_Virtuale.php

---
+++ Mini_Biblioteca_Virtuale.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <h1>Mini Biblioteca Virtuale</h1>
+        <body>
+            <form action = "calcolatrice.php" method = "post" enctype = "multipart/form*data">
+                Seleziona file PDF:
+                <input type = "file" name = "filePDF" id = "filePDF">
+                <input type = "submit" value = "CARICA" name = "submit">
+            </form>
+            <?php
+                $uploadDir = __DIR__. '\calcolatrice';
+
+                foreach ($_FILES as files) {
+                    if (UPLOAD_ERR_OK === $file['error']){
+                        $fileName = basename($file['name']);
+                        move_uploaded_file(
+                            $file['tmp_name'],
+                            $uploadDir.DIRECTORY_SEPARATOR.$fileName
+                        );
+                    }
+                }
+            ?>
+        </body>
+    </head>
+</html>
