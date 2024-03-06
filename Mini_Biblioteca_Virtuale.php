ee30a608081d667a73b9d8e07c34ce9709a9eea8
AnthonyCorrentini <162418142+AnthonyCorrentini@users.noreply.github.com>
2024-03-06 09:06:43 +0100

    Update Mini_Biblioteca_Virtuale.php

--- Mini_Biblioteca_Virtuale.php
+++ Mini_Biblioteca_Virtuale.php
@@ -3,20 +3,20 @@
     <head>
         <h1>Mini Biblioteca Virtuale</h1>
         <body>
-            <form action = "Uploads.php" method = "post" enctype = "multipart/form*data">
+            <form action = "Uploads.php" method = "post" enctype = "multipart/form-data">
                 Seleziona file PDF:
                 <input type = "file" name = "filePDF" id = "filePDF">
                 <input type = "submit" value = "CARICA" name = "submit">
             </form>
             <?php
-                $uploadDir = __DIR__. '\Uploads';
+                $uploadDir = __DIR__.'\Uploads';
 
-                foreach ($_FILES as files) {
+                foreach ($_FILES as $file) {
                     if (UPLOAD_ERR_OK === $file['error']){
                         $fileName = basename($file['name']);
                         move_uploaded_file(
                             $file['tmp_name'],
-                            $uploadDir.DIRECTORY_SEPARATOR.$fileName
+                           $uploadDir.DIRECTORY_SEPARATOR.$fileName
                         );
                     }
                 }
